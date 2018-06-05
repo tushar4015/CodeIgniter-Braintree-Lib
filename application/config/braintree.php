74fd2930d482697c0a595e467bfb9b14d5150876
tushar4015 <37686522+tushar4015@users.noreply.github.com>
2018-06-05 15:09:43 +0530

    change sandbox spelling in config file
    i just change one spelling mistake in config file of sandbook to sandbox

--- application/config/braintree.php
+++ application/config/braintree.php
@@ -4,4 +4,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   $config['braintree_merchant_id'] = '';
   $config['braintree_public_key']  = '';
   $config['braintree_private_key'] = '';
-  $config['braintree_environment'] = ''; //production or sandbok
+  $config['braintree_environment'] = ''; //production or sandbox
